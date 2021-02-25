136a78c050c1b9e97e8389df83462de3542e98ac
Javed Khan <javed.khan@resourcifi.com>
2021-02-25 16:59:32 +0530

    Min SIze

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -1,4 +1,12 @@
 // Call the dataTables jQuery plugin
-$(document).ready(function() {
-  $('.dataTable-view').DataTable();
+// $(document).ready(function() {
+//   $('.dataTable-view').DataTable();
+// });
+
+
+$('.dataTable-view').dataTable({
+    "paging": false,
+    "ordering": false,
+    "info": false,
+    "searching": false
 });
