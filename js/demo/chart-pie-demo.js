3621e8c071ca9d7343a9f667f14b13fa00ba0bfb
Javed Khan <javed.khan@resourcifi.com>
2021-03-01 15:44:45 +0530

    Chart - Added

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -36,68 +36,137 @@ Chart.defaults.global.defaultFontColor = '#858796';
 
 var ctx = document.getElementById("myPieChart").getContext('2d');
 var myPieChart = new Chart(ctx, {
-  type: 'pie',
-  data: {
-    labels: ["Submitted", "Delivered", "Failed"],
-    datasets: [{
-      backgroundColor: [
-        "#f08c41",
-        "#18634B",
-        "#A91E2C"
-      ],
-      data: [12975, 19335, 3663]
-    }]
-  },
-  options: {
+    type: 'pie',
+    data: {
+        labels: ["Client ID", "SMPP A/C", "No of SMS"],
+        datasets: [{
+            backgroundColor: [
+                "#37a2eb",
+                "#1cc88a",
+                "#ff6384"
+            ],
+            data: [12975, 19335, 3663]
+        }]
+    },
+    options: {
         maintainAspectRatio: false,
         tooltips: {
-          backgroundColor: "rgb(255,255,255)",
-          bodyFontColor: "#858796",
-          borderColor: '#dddfeb',
-          borderWidth: 1,
-          xPadding: 15,
-          yPadding: 15,
-          displayColors: false,
-          caretPadding: 10,
+            backgroundColor: "rgb(255,255,255)",
+            bodyFontColor: "#858796",
+            borderColor: '#dddfeb',
+            borderWidth: 1,
+            xPadding: 15,
+            yPadding: 15,
+            displayColors: false,
+            caretPadding: 10,
         },
         legend: {
-          display: false
+            display: false
         },
-        cutoutPercentage: 30,
-      },
+        cutoutPercentage: 50,
+    },
 });
 
 
 
 var ctx = document.getElementById("myPieChart1").getContext('2d');
 var myPieChart1 = new Chart(ctx, {
-  type: 'pie',
-  data: {
-    labels: ["Link Created", "Clicks", "SMS Clicks"],
-    datasets: [{
-      backgroundColor: [
-        "#f08c41",
-        "#18634B",
-        "#acacaf"
-      ],
-      data: [12975, 19335, 3663]
-    }]
-  },
-  options: {
-    maintainAspectRatio: false,
-    tooltips: {
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      caretPadding: 10,
+    type: 'pie',
+    data: {
+        labels: ["Client ID", "SMS Submitted", "Delivered", "Failed", "Unknown"],
+        datasets: [{
+            backgroundColor: [
+                "#37a2eb",
+                "#f8ce6b",
+                "#1cc88a",
+                "#ff6384",
+                "#8e8e8e"
+            ],
+            data: [12975, 19335, 3663, 77887, 8880]
+        }]
     },
-    legend: {
-      display: false
+    options: {
+        maintainAspectRatio: false,
+        tooltips: {
+            backgroundColor: "rgb(255,255,255)",
+            bodyFontColor: "#858796",
+            borderColor: '#dddfeb',
+            borderWidth: 1,
+            xPadding: 15,
+            yPadding: 15,
+            displayColors: false,
+            caretPadding: 10,
+        },
+        legend: {
+            display: false
+        },
+        cutoutPercentage: 30,
     },
-    cutoutPercentage: 30,
-  },
+});
+
+var ctx = document.getElementById("myPieChart2").getContext('2d');
+var myPieChart1 = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ["SMPP Provider", "Total SMS Submitted ", "Unknown Del"],
+        datasets: [{
+            backgroundColor: [
+                "#37a2eb",
+                "#f8ce6b",
+                "#1cc88a"
+            ],
+            data: [12975, 19335, 3663]
+        }]
+    },
+    options: {
+        maintainAspectRatio: false,
+        tooltips: {
+            backgroundColor: "rgb(255,255,255)",
+            bodyFontColor: "#858796",
+            borderColor: '#dddfeb',
+            borderWidth: 1,
+            xPadding: 15,
+            yPadding: 15,
+            displayColors: false,
+            caretPadding: 10,
+        },
+        legend: {
+            display: false
+        },
+        cutoutPercentage: 30,
+    },
+});
+
+
+var ctx = document.getElementById("myPieChart3").getContext('2d');
+var myPieChart1 = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ["Error Wise", "Oprator Wise", "Client Wise"],
+        datasets: [{
+            backgroundColor: [
+                "#1cc88a",
+                "#ff6384",
+                "#8e8e8e"
+            ],
+            data: [3663, 77887, 8880]
+        }]
+    },
+    options: {
+        maintainAspectRatio: false,
+        tooltips: {
+            backgroundColor: "rgb(255,255,255)",
+            bodyFontColor: "#858796",
+            borderColor: '#dddfeb',
+            borderWidth: 1,
+            xPadding: 15,
+            yPadding: 15,
+            displayColors: false,
+            caretPadding: 10,
+        },
+        legend: {
+            display: false
+        },
+        cutoutPercentage: 30,
+    },
 });
